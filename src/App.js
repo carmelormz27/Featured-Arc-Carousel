5ce97a961c7b5c5112828b1fe9126fb21cf82a3c
Carmelo Ramirez <jose.ramirez@wizeline.com>
2022-03-14 10:57:42 -0600

    Carousel v2

--- src/App.js
+++ src/App.js
@@ -1,116 +1,208 @@
-import FeaturedArcCarousel from './FeaturedArcCarousel';
+import { useState, useEffect } from 'react';
+import FeaturedArcCarousel from './Components/Featured Arc Carouse/FeaturedArcCarousel';
 
-const slides = [
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61ba68f33c9ac16401dad18a_maria_mercedes.png',
-    title: 'Maria Mercedes',
-    category: 'Novela',
-    year: '2005',
-    index: 0,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce43264646fda2bd39_Me%20Robo%20Mi%20Vida.png',
-    title: 'Me Robo Mi Vida',
-    category: 'Novela',
-    year: '2007',
-    index: 1,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fceac05f3857b109887_Muerte%20En%20Buenos%20Aires.png',
-    title: 'Muerte En Buenos Aires',
-    category: 'Pelicula',
-    year: '2008',
-    index: 2,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fcfc58ab47375e6c5e7_Rubi.png',
-    title: 'Rubi',
-    category: 'Novela',
-    year: '2009',
-    index: 3,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fceb5bd4e104e39a84f_La%20Muerte%20De%20Marga%20Maier.png',
-    title: 'La Muerte de Marga Maier',
-    category: 'Novela',
-    year: '2010',
-    index: 4,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fcec098635686af928d_Teresa.png',
-    title: 'Teresa',
-    category: 'Novela',
-    year: '2001',
-    index: 5,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fcee748395ac11cae74_Solteras.png',
-    title: 'Solteras',
-    category: 'Novela',
-    year: '2015',
-    index: 6,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e746d847f416d695b2c55d_Enamorandome%20de%20Ramon.png',
-    title: 'Enamorandome de Ramón',
-    category: 'Novela',
-    year: '2007',
-    index: 7,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce23f885433306f42f_La%20Chica%20que%20Limpia.png',
-    title: 'La Chica Que Limpia',
-    category: 'Novela',
-    year: '2010',
-    index: 8,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fceaf71ee0c37590c5a_Rescue%20Dawn.png',
-    title: 'Rescue Dawn',
-    category: 'Pelicula',
-    year: '2001',
-    index: 9,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce95afa656220ffdfd_La%20Familia%20P.Luche.png',
-    title: 'La Familia Peluche',
-    category: 'Programa',
-    year: '2006',
-    index: 10,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61f02ace8aab3079b807b861_Amore%20Manchado.png',
-    title: 'Amor Manchado',
-    category: 'Novela',
-    year: '2008',
-    index: 11,
-  },
-  {
-    imageUrl:
-      'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce6e40bb36e4c49d5c_Harsh%20Times.png',
-    title: 'Harsh Times',
-    category: 'Pelicula',
-    year: '2011',
-    index: 12,
-  },
-];
+// const slides = [
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61ba68f33c9ac16401dad18a_maria_mercedes.png',
+//     title: 'Maria Mercedes',
+//     category: 'Novela',
+//     year: '2005',
+//     index: 0,
+//     url: 'https://www.google.com/',
+//     subtitle: '2005 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce43264646fda2bd39_Me%20Robo%20Mi%20Vida.png',
+//     title: 'Me Robo Mi Vida',
+//     category: 'Novela',
+//     year: '2007',
+//     index: 1,
+//     url: '',
+//     subtitle: '2007 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fceac05f3857b109887_Muerte%20En%20Buenos%20Aires.png',
+//     title: 'Muerte En Buenos Aires',
+//     category: 'Pelicula',
+//     year: '2008',
+//     index: 2,
+//     url: '',
+//     subtitle: '2008 • Pelicula',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fcfc58ab47375e6c5e7_Rubi.png',
+//     title: 'Rubi',
+//     category: 'Novela',
+//     year: '2009',
+//     index: 3,
+//     url: '',
+//     subtitle: '2009 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fceb5bd4e104e39a84f_La%20Muerte%20De%20Marga%20Maier.png',
+//     title: 'La Muerte de Marga Maier',
+//     category: 'Novela',
+//     year: '2010',
+//     index: 4,
+//     url: '',
+//     subtitle: '2010 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fcec098635686af928d_Teresa.png',
+//     title: 'Teresa',
+//     category: 'Novela',
+//     year: '2001',
+//     index: 5,
+//     url: '',
+//     subtitle: '2001 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fcee748395ac11cae74_Solteras.png',
+//     title: 'Solteras',
+//     category: 'Novela',
+//     year: '2015',
+//     index: 6,
+//     url: '',
+//     subtitle: '2015 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e746d847f416d695b2c55d_Enamorandome%20de%20Ramon.png',
+//     title: 'Enamorandome de Ramón',
+//     category: 'Novela',
+//     year: '2007',
+//     index: 7,
+//     url: '',
+//     subtitle: '2007 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce23f885433306f42f_La%20Chica%20que%20Limpia.png',
+//     title: 'La Chica Que Limpia',
+//     category: 'Novela',
+//     year: '2010',
+//     index: 8,
+//     url: '',
+//     subtitle: '2010 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fceaf71ee0c37590c5a_Rescue%20Dawn.png',
+//     title: 'Rescue Dawn',
+//     category: 'Pelicula',
+//     year: '2001',
+//     index: 9,
+//     url: '',
+//     subtitle: '2001 • Pelicula',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce95afa656220ffdfd_La%20Familia%20P.Luche.png',
+//     title: 'La Familia Peluche',
+//     category: 'Programa',
+//     year: '2006',
+//     index: 10,
+//     url: '',
+//     subtitle: '2006 • Programa',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61f02ace8aab3079b807b861_Amore%20Manchado.png',
+//     title: 'Amor Manchado',
+//     category: 'Novela',
+//     year: '2008',
+//     index: 11,
+//     url: '',
+//     subtitle: '2008 • Novela',
+//   },
+//   {
+//     imageUrl:
+//       'https://assets.website-files.com/61b0ce185c49285b8549f807/61e73fce6e40bb36e4c49d5c_Harsh%20Times.png',
+//     title: 'Harsh Times',
+//     category: 'Pelicula',
+//     year: '2011',
+//     index: 12,
+//     url: '',
+//     subtitle: '2011 • Pelicula',
+//   },
+// ];
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
 const App = () => {
-  const carouselSlides = window.carouselSlides ? window.carouselSlides : [];
+  const slides = window.carouselSlides || [];
 
-  return <FeaturedArcCarousel slides={slides} />;
+  const [carouselSlides, setCarouselSlides] = useState(
+    slides.slice(0, slides.length)
+  );
+  const [windowsDimension, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const getDiameter = () => {
+    if (windowsDimension.width >= 768) {
+      return (
+        Math.round(windowsDimension.width) * 1.1458 +
+        Math.round(windowsDimension.width * 0.174) * 1.5
+      );
+    }
+
+    return (
+      clamp(800, windowsDimension.width * 3.2401, 1215) +
+      Math.round(windowsDimension.width * 0.5) * 1.5
+    );
+  };
+
+  const diameter = getDiameter();
+
+  useEffect(() => {
+    const auxArray = slides.slice(0, slides.length);
+    if (slides.length < 36) {
+      const initialSize = slides.length;
+      const missing = 36 - slides.length;
+      let i = 0;
+      let currentIndex = 0;
+      while (i < missing) {
+        auxArray.push(slides[currentIndex]);
+        currentIndex === initialSize - 1 ? (currentIndex = 0) : currentIndex++;
+        i++;
+      }
+
+      setCarouselSlides(auxArray);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  });
+
+  return (
+    <>
+      <FeaturedArcCarousel
+        slides={carouselSlides}
+        degrees={10}
+        diameter={diameter}
+        initialSlidesLength={slides.length}
+      />
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/Featured Arc Carouse/FeaturedArcCarousel.js
@@ -0,0 +1,185 @@
+import { motion, useMotionValue } from 'framer-motion';
+import { useState, useRef, useEffect } from 'react';
+
+import './FeaturedArcCarousel.css';
+
+const FeaturedArcCarousel = ({
+  slides,
+  degrees,
+  diameter,
+  initialSlidesLength,
+}) => {
+  const slideWidth = diameter / slides.length;
+  const centerIndex = 0;
+
+  const slidesRef = useRef([]);
+  const progressBarRef = useRef(null);
+  const paginationBarRef = useRef(null);
+
+  const [isDragging, setIsDragging] = useState(false);
+  const [active, setActive] = useState(0);
+  const [coordX, setCoordX] = useState(0);
+
+  const [cardTitle, setCardTitle] = useState(slides[centerIndex].title);
+  const [cardDetail, setCardDetail] = useState(slides[centerIndex].subtitle);
+  const [cardUrl, setCardUrl] = useState(slides[centerIndex].url);
+  const [paginationPosition, setPaginationPosition] = useState(0);
+  const [paginationWidth, setPaginationWidth] = useState(0);
+
+  const onUpdate = (latest) => {
+    const activeIndex = getActiveIndex(slidesRef.current);
+    setPaginationPosition(paginationWidth * slides[activeIndex].index);
+    setCoordX(latest.x);
+  };
+
+  const handleModifyTarget = (target) => {
+    const snapTarget = Math.round(target / slideWidth) * slideWidth;
+    return snapTarget;
+  };
+
+  const getActiveIndex = () => {
+    return slidesRef.current.findIndex((slide, index) => {
+      const deg =
+        getComputedStyle(slide).getPropertyValue('-webkit-transform') ||
+        getComputedStyle(slide).getPropertyValue('-moz-transform') ||
+        getComputedStyle(slide).getPropertyValue('-ms-transform') ||
+        getComputedStyle(slide).getPropertyValue('-o-transform') ||
+        getComputedStyle(slide).getPropertyValue('transform');
+
+      if (deg === 'none') {
+        return true;
+      }
+
+      const values = deg.split('(')[1].split(')')[0].split(',');
+      const angle = Math.round(
+        Math.atan2(values[1], values[0]) * (180 / Math.PI)
+      );
+
+      return (
+        Math.abs(angle) < 10 || Math.abs(angle) === 0 || Math.abs(angle) === 360
+      );
+    });
+  };
+
+  const buttonClickHandler = () => {
+    if (window.featuredCarouselAnalytics?.buttonClick) {
+      const activeIndex = getActiveIndex();
+
+      window.featuredCarouselAnalytics.buttonClick(
+        slides[activeIndex].index,
+        slides[activeIndex].title,
+        slides[activeIndex].url
+      );
+    }
+  };
+
+  const triggerSwapAnalyticsEvent = () => {
+    if (window.featuredCarouselAnalytics?.slideSwaped) {
+      const activeIndex = getActiveIndex();
+
+      window.featuredCarouselAnalytics.slideSwaped(
+        slides[activeIndex].index,
+        slides[activeIndex].title,
+        slides[activeIndex].url
+      );
+    }
+  };
+
+  useEffect(() => {
+    slidesRef.current = slidesRef.current.slice(0, slides.length);
+  }, [slidesRef]);
+
+  useEffect(() => {
+    const activeIndex = getActiveIndex();
+
+    if (!isDragging) {
+      setActive(activeIndex);
+      setCardTitle(slides[activeIndex].title);
+      setCardDetail(slides[activeIndex].subtitle);
+      setCardUrl(slides[activeIndex].url);
+
+      triggerSwapAnalyticsEvent();
+    }
+    setPaginationPosition(paginationWidth * slides[activeIndex].index);
+  }, [isDragging]);
+
+  useEffect(() => {
+    setPaginationWidth(
+      progressBarRef.current?.offsetWidth / initialSlidesLength
+    );
+  }, [diameter]);
+
+  return (
+    <div className="carousel-root">
+      <div className="carousel-container">
+        <motion.div
+          drag="x"
+          onUpdate={onUpdate}
+          dragElastic={0}
+          dragTransition={{
+            power: 0.05,
+            timeConstant: 0,
+            modifyTarget: handleModifyTarget,
+          }}
+        >
+          <div className="carousel-cards-container">
+            {slides.map((slide, index) => {
+              const isActive = active === index;
+              const rotate = (index - centerIndex) * degrees;
+              const divider = slideWidth / degrees;
+
+              return (
+                <motion.div
+                  key={index}
+                  ref={(el) => (slidesRef.current[index] = el)}
+                  className={`carousel-card ${isActive ? 'active' : ''}`}
+                  initial={{
+                    rotate: coordX / divider + rotate,
+                    transformOrigin: `50% ${diameter}px`,
+                  }}
+                  animate={{
+                    rotate: coordX / divider + rotate,
+                    transformOrigin: `50% ${diameter}px`,
+                  }}
+                  transition={{
+                    ease: 'easeOut',
+                    duration: 0.3,
+                  }}
+                  onAnimationStart={() => setIsDragging(true)}
+                  onAnimationComplete={() => setIsDragging(false)}
+                >
+                  <img src={slide.imageUrl} alt="" />
+                </motion.div>
+              );
+            })}
+          </div>
+        </motion.div>
+        <div className="carousel-content">
+          <p className="feature-card-title label-2--medium">{cardTitle}</p>
+          <p className="label-4 feature-card-detail">{cardDetail}</p>
+          <a
+            className="large-button featured-carousel-cta"
+            href={cardUrl}
+            target="_blank"
+            rel="noreferrer"
+            onClick={buttonClickHandler}
+          >
+            Ver Ahora
+          </a>
+        </div>
+      </div>
+      <div ref={progressBarRef} className="featured-carousel-progress">
+        <div
+          ref={paginationBarRef}
+          className="featued-carousel-pagination"
+          style={{
+            width: `${paginationWidth}px`,
+            marginLeft: `${paginationPosition}px`,
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedArcCarousel;
